9b6fee92497b501a318c1e4a21fd4778ffb7580f
daya2940 <sauuit2940@gmail.com>
2021-12-06 23:23:33 +0530

    Added seeder script

---
+++ data/users.js
@@ -0,0 +1,21 @@
+import bcrypt from "bcryptjs";
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John doe",
+    email: "john@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Jane",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
