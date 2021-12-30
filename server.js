0796f86669c23477c6546e526ff3ab1f24b38cd3
daya2940 <sauuit2940@gmail.com>
2021-12-31 00:31:31 +0530

    Code refractored

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
-import products from "./data/product.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import colors from "colors";
+import productRoutes from "./routes/productRouter.js";
 
 dotenv.config();
 
@@ -14,14 +14,7 @@ app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const productItem = products.find((item) => item._id === req.params.id);
-  res.json(productItem);
-});
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
