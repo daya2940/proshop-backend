3cdc58066074d61dd2254b1b505608440b866389
daya2940 <sauuit2940@gmail.com>
2022-02-05 23:07:51 +0530

    Added user register and user profile functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,24 +22,53 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @description Register user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid UserData");
+  }
+});
+
 // @description Get user profile
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
   res.send("success");
-  // const user = await User.findOne({ email: email });
-  // if (user && (await user.matchPassword(password))) {
-  //   res.json({
-  //     _id: user._id,
-  //     name: user.name,
-  //     email: user.email,
-  //     isAdmin: user.isAdmin,
-  //     token: generateToken(user._id),
-  //   });
-  // } else {
-  //   res.status(401);
-  //   throw new Error("Invalid email or password");
-  // }
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
